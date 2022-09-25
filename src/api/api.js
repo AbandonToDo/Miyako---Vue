8a418b2f5352c329da82e6ea48d882fb57f715b1
CN1\smyan <smyan@trip.com>
2022-09-25 18:48:14 +0800

    update[就添加了删除学生信息的api] ref:20220925

--- src/api/api.js
+++ src/api/api.js
@@ -1,35 +1,35 @@
-import service from '../service';
+import service from '../service'
 
 // 登陆API
 export function login(data) {
   return service({
     url: '/login',
     method: 'post',
-    data,
-  });
+    data
+  })
 }
 // 学生列表查询API
 export function getStudentList(params) {
   return service({
     url: `/students?name=${params}`,
     method: 'get',
-    params,
-  });
+    params
+  })
 }
 // 学生信息删除
 export function deleteStudent(id) {
   return service({
     url: `/students/${id}`,
-    method: 'delete',
-  });
+    method: 'delete'
+  })
 }
 // 学生详细信息列表
 export function getStudentInfo(params) {
   return service({
     url: '/info',
     method: 'get',
-    params,
-  });
+    params
+  })
 }
 
 // 增加学生信息
@@ -37,6 +37,14 @@ export function addStudentInfo(data) {
   return service({
     url: '/info',
     method: 'post',
-    data,
-  });
+    data
+  })
+}
+
+// 删除学生信息
+export function deleteStudentInfo(id) {
+  return service({
+    url: `/info/${id}`,
+    method: 'delete'
+  })
 }
